dcbd043a81fc31c18948d67d90f80a3af66ada7f
Happycupra <happymeal25@proton.me>
2026-09-16 10:06:17 +0200

    Refresh order coverage with day planning changes

--- src/Produktionsplanung.App/Views/DayPlanningView.xaml.cs
+++ src/Produktionsplanung.App/Views/DayPlanningView.xaml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Windows.Controls;
 using Produktionsplanung.App.ViewModels;
 
@@ -8,6 +9,16 @@ public partial class DayPlanningView : UserControl
     public DayPlanningView()
     {
         InitializeComponent();
-        DataContext = new DayPlanningViewModel();
+        var viewModel = new DayPlanningViewModel();
+        viewModel.PropertyChanged += ViewModel_PropertyChanged;
+        DataContext = viewModel;
+        viewModel.RefreshProductionOrderCoverage();
+    }
+
+    private static void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (sender is not DayPlanningViewModel viewModel) return;
+        if (e.PropertyName is nameof(DayPlanningViewModel.SelectedDate) or nameof(DayPlanningViewModel.StatusMessage))
+            viewModel.RefreshProductionOrderCoverage();
     }
 }
