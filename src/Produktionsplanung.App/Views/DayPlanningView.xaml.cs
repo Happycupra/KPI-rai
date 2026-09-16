231e6a540de32ac2f06dc642a3741b54c0ef2c0e
Happycupra <happymeal25@proton.me>
2026-09-16 15:37:58 +0200

    Make employee names open quick card in day planning

--- src/Produktionsplanung.App/Views/DayPlanningView.xaml.cs
+++ src/Produktionsplanung.App/Views/DayPlanningView.xaml.cs
@@ -1,5 +1,9 @@
 using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Produktionsplanung.App.Services;
 using Produktionsplanung.App.ViewModels;
 
 namespace Produktionsplanung.App.Views;
@@ -19,7 +23,63 @@ public partial class DayPlanningView : UserControl
         };
         viewModel.PropertyChanged += ViewModel_PropertyChanged;
         DataContext = viewModel;
+        PreviewMouseLeftButtonUp += EmployeeName_PreviewMouseLeftButtonUp;
+        PreviewMouseMove += EmployeeName_PreviewMouseMove;
+        MouseLeave += (_, _) => Cursor = Cursors.Arrow;
         RefreshSupplementalPlanningData(viewModel);
+    }
+
+    private void EmployeeName_PreviewMouseMove(object sender, MouseEventArgs e)
+    {
+        Cursor = TryGetEmployee(e.OriginalSource as DependencyObject, out _, out _)
+            ? Cursors.Hand
+            : Cursors.Arrow;
+    }
+
+    private void EmployeeName_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        if (!TryGetEmployee(e.OriginalSource as DependencyObject, out var employeeId, out var employeeName))
+            return;
+
+        if (Application.Current.MainWindow is MainWindow mainWindow && DataContext is DayPlanningViewModel viewModel)
+        {
+            mainWindow.OpenEmployeeQuickCard(employeeId, viewModel.SelectedDate);
+            e.Handled = true;
+        }
+    }
+
+    private static bool TryGetEmployee(DependencyObject? source, out int employeeId, out string employeeName)
+    {
+        employeeId = 0;
+        employeeName = string.Empty;
+        var textBlock = FindAncestor<TextBlock>(source);
+        if (textBlock is null)
+            return false;
+
+        switch (textBlock.DataContext)
+        {
+            case DayAssignmentRow row when string.Equals(textBlock.Text, row.EmployeeName, StringComparison.Ordinal):
+                employeeId = row.EmployeeId;
+                employeeName = row.EmployeeName;
+                return true;
+            case EmployeeSuggestion suggestion when string.Equals(textBlock.Text, suggestion.EmployeeName, StringComparison.Ordinal):
+                employeeId = suggestion.EmployeeId;
+                employeeName = suggestion.EmployeeName;
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private static T? FindAncestor<T>(DependencyObject? current) where T : DependencyObject
+    {
+        while (current is not null)
+        {
+            if (current is T match)
+                return match;
+            current = VisualTreeHelper.GetParent(current);
+        }
+        return null;
     }
 
     private static void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
