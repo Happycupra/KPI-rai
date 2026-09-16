1a72783891a9e6d99ce681f006042d0ee425a1b1
Happycupra <happymeal25@proton.me>
2026-09-16 14:11:24 +0200

    Allow calendar to open selected planning day

--- src/Produktionsplanung.App/Views/DayPlanningView.xaml.cs
+++ src/Produktionsplanung.App/Views/DayPlanningView.xaml.cs
@@ -6,19 +6,32 @@ namespace Produktionsplanung.App.Views;
 
 public partial class DayPlanningView : UserControl
 {
-    public DayPlanningView()
+    public DayPlanningView() : this(DateTime.Today)
+    {
+    }
+
+    public DayPlanningView(DateTime initialDate)
     {
         InitializeComponent();
-        var viewModel = new DayPlanningViewModel();
+        var viewModel = new DayPlanningViewModel
+        {
+            SelectedDate = initialDate.Date
+        };
         viewModel.PropertyChanged += ViewModel_PropertyChanged;
         DataContext = viewModel;
         viewModel.RefreshProductionOrderCoverage();
+        viewModel.RefreshSkillAlerts();
+        viewModel.RefreshEmployeeSuggestions();
     }
 
     private static void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
         if (sender is not DayPlanningViewModel viewModel) return;
         if (e.PropertyName is nameof(DayPlanningViewModel.SelectedDate) or nameof(DayPlanningViewModel.StatusMessage))
+        {
             viewModel.RefreshProductionOrderCoverage();
+            viewModel.RefreshSkillAlerts();
+            viewModel.RefreshEmployeeSuggestions();
+        }
     }
 }
