21afc9e392129d46dcd1c374371115b7058660ba
Happycupra <happymeal25@proton.me>
2026-09-16 10:31:26 +0200

    Refresh OEE analytics with KPI period

--- src/Produktionsplanung.App/Views/AnalyticsView.xaml.cs
+++ src/Produktionsplanung.App/Views/AnalyticsView.xaml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Windows.Controls;
 using Produktionsplanung.App.ViewModels;
 
@@ -5,9 +6,20 @@ namespace Produktionsplanung.App.Views;
 
 public partial class AnalyticsView : UserControl
 {
+    private readonly AnalyticsViewModel viewModel;
+
     public AnalyticsView()
     {
         InitializeComponent();
-        DataContext = new AnalyticsViewModel();
+        viewModel = new AnalyticsViewModel();
+        DataContext = viewModel;
+        viewModel.PropertyChanged += ViewModel_PropertyChanged;
+        viewModel.RefreshOeeAnalytics();
+    }
+
+    private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(AnalyticsViewModel.PeriodText))
+            viewModel.RefreshOeeAnalytics();
     }
 }
