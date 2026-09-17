c644d44f892447a0d86129e9d0f1a86846e02c60
Happycupra <happymeal25@proton.me>
2026-09-17 15:33:50 +0200

    Protect unsaved employee edits during navigation

--- src/Produktionsplanung.App/Views/EmployeesView.xaml.cs
+++ src/Produktionsplanung.App/Views/EmployeesView.xaml.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel;
 using System.Windows.Controls;
+using Produktionsplanung.App.Services;
 using Produktionsplanung.App.ViewModels;
 
 namespace Produktionsplanung.App.Views;
 
-public partial class EmployeesView : UserControl
+public partial class EmployeesView : UserControl, IUnsavedChangesAware
 {
+    private readonly EmployeeManagementViewModel viewModel;
+    private string baseline = string.Empty;
+
     public EmployeesView() : this(null)
     {
     }
@@ -12,9 +17,54 @@ public partial class EmployeesView : UserControl
     public EmployeesView(int? employeeId)
     {
         InitializeComponent();
-        var viewModel = new EmployeeManagementViewModel();
+        viewModel = new EmployeeManagementViewModel();
         if (employeeId.HasValue)
             viewModel.SelectEmployeeById(employeeId.Value);
         DataContext = viewModel;
+        viewModel.PropertyChanged += ViewModel_PropertyChanged;
+        CaptureBaseline();
     }
+
+    public bool HasUnsavedChanges => baseline != BuildSnapshot();
+    public string UnsavedChangesDescription => "Mitarbeiterdaten";
+
+    public bool TrySaveChanges()
+    {
+        viewModel.SaveCommand.Execute(null);
+        return !HasUnsavedChanges;
+    }
+
+    public void DiscardChanges()
+    {
+        var id = viewModel.EditingId;
+        if (id > 0)
+        {
+            viewModel.SelectedEmployee = null;
+            viewModel.SelectEmployeeById(id);
+        }
+        else
+        {
+            viewModel.NewEmployeeCommand.Execute(null);
+        }
+        CaptureBaseline();
+    }
+
+    private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(EmployeeManagementViewModel.SelectedEmployee) or nameof(EmployeeManagementViewModel.EditingId))
+            CaptureBaseline();
+    }
+
+    private void CaptureBaseline() => baseline = BuildSnapshot();
+
+    private string BuildSnapshot() => string.Join("\u001f",
+        viewModel.EditingId,
+        viewModel.PersonnelNumber,
+        viewModel.FirstName,
+        viewModel.LastName,
+        viewModel.Role,
+        viewModel.Department,
+        viewModel.WorkloadPercent,
+        viewModel.WeeklyTargetHours,
+        viewModel.IsActive);
 }
